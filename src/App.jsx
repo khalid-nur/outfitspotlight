bdb28ece29902b6f228307d6ea25039c9a098f5b
khalid-nur <khalidnur150@gmail.com>
2023-08-13 21:50:41 -0500

    added routing guard to restrict unauthorized user access to core pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+import Loader from "./components/Loader/Loader";
 
 // Pages
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import HomeLayout from "./Layouts/HomeLayout";
 
 const App = () => {
+  const { authIsReady, user } = useAuthContext();
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady ? (
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={!user ? <Login /> : <Navigate to="/home" />}
+              // If there is no user logged in, direct them to the Login page.
+              // Otherwise, redirect to the Home page.
+            />
+            <Route path="/signup" element={<Register />} />
+
+            <Route
+              path="/home"
+              element={user ? <HomeLayout /> : <Navigate to="/" />}
+              // If a user is logged in, direct them to the Home page.
+              // Otherwise, redirect to the Login page.
+            />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../pages/Home";
+import Navbar from "../components/Navbar/Navbar";
+
+const HomeLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+};
+
+export default HomeLayout;
