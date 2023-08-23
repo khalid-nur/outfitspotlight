e9f3992f12473e36f69a0462c712d3e2afc3d29b
khalid-nur <khalidnur150@gmail.com>
2023-08-22 22:35:55 -0500

    implemented explore page layout for exploring user posts and integrated router for navigation

---
+++ src/Layouts/ExploreLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Explore from "../pages/Explore";
+
+const ExploreLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Explore />
+    </>
+  );
+};
+
+export default ExploreLayout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PostFeed from "../components/PostFeed/PostFeed";
+import Post from "../components/Post/Post";
 
 const Home = () => {
   return (
     <div>
       <PostFeed />
+      <Post />
     </div>
   );
 };
