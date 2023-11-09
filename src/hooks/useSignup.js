f46c5819884e5474291124f02a48d3ce0985276a
khalid-nur <khalidnur150@gmail.com>
2023-11-08 18:52:37 -0600

    update user signup doc. removed darkMode field, added webUrl to include URL link in users bio

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -63,7 +63,7 @@ export const useSignup = () => {
         userEmail: userEmail,
         bio: "",
         timestamp: serverTimestamp(),
-        darkMode: false,
+        webUrl: "",
       });
 
       // Dispatch login action
