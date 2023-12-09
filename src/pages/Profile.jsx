d6cc38d993d10f64a92f040cd8611b803f6c1f34
khalid-nur <khalidnur150@gmail.com>
2023-12-09 16:00:09 -0600

    Added counters for posts, followers, and following

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,29 +9,51 @@ import DefaultAvatar from "../assets/signup-default-avatar.png";
 import { useAuthContext } from "../hooks/useAuthContext";
 import FollowButton from "../components/FollowButton/FollowButton";
 import EditProfile from "../components/EditProfile/EditProfile";
+import SkeletonProfile from "../components/Skeleton/SkeletonProfile";
 
 function Profile() {
   // State to hold the user profile data
   const [currentUserProfile, setCurrentUserProfile] = useState(null);
-  // Fetch user data and post data for firebase
-  const { documents: userDocs } = useCollection("users");
-  const { documents } = useCollection("posts");
+  const [followerCount, setFollowerCount] = useState(0);
+  const [followingCount, setFollowingCount] = useState(0);
+  const [postCount, setPostCount] = useState(0);
   const { user } = useAuthContext();
+  // Fetching users, posts, and follows data
+  const { documents: userDocs, isPending } = useCollection("users");
+  const { documents: postDocs } = useCollection("posts");
+  const { documents: followDocs } = useCollection("follows");
 
   // Get the 'id' parameter from the URL (this should have id of the users Id for URL)
   const { id } = useParams();
-  console.log(user.uid);
-  console.log(id);
 
-  // useEffect to update the user profile data when 'userDocs' or 'id' changes
   useEffect(() => {
     // Find the current user's data based on 'id'
-    const currentUser = userDocs?.find((userDoc) => userDoc?.userId === id);
+    const currentProfile = userDocs?.find((userDoc) => userDoc.userId === id);
+    // Filtering to find users that the current user is following.
+    const userFollowing = followDocs?.filter(
+      (follows) => follows.currentUserId === id
+    );
+    // Filtering to find users that are following the current user.
+    const userFollowers = followDocs?.filter(
+      (follows) => follows.postUserId === id
+    );
+    // Filtering to find posts made by the current user.
+    const userPosts = postDocs?.filter((post) => post.userId === id);
+    console.log(userPosts);
 
-    // Update the 'currentUserProfile' state
-    setCurrentUserProfile(currentUser);
-    return () => {};
-  }, [userDocs, id]);
+    setCurrentUserProfile(currentProfile); // Updating the 'currentUserProfile' state by setting the current user's profile.
+    setFollowingCount(userFollowing?.length); // Setting the number of users the current user is following.
+    setFollowerCount(userFollowers?.length); // Setting the number of followers of the current user.
+    setPostCount(userPosts?.length); // Setting the number of posts by the current user
+  }, [userDocs, id, followDocs]);
+
+  console.log("following: " + followingCount);
+  console.log("followers: " + followerCount);
+  console.log("post: " + postCount);
+
+  if (isPending) {
+    return <SkeletonProfile />;
+  }
 
   return (
     <>
@@ -40,14 +62,14 @@ function Profile() {
           {/* <!-- Profile picture and edit button --> */}
           <div className="flex items-center justify-between px-4 py-3">
             <img
-              className="h-32 w-32 md:h-44 md:w-44 cursor-pointer rounded-full"
+              className="h-32 w-32 md:h-44 md:w-44 cursor-pointer rounded-full  bg-[#efefef]"
               src={currentUserProfile?.photoURL ?? DefaultAvatar}
             />
 
             <div className=" hidden items-center gap-6 md:flex">
               <div className="flex flex-col items-center text-center">
                 <span className=" text-lg font-semibold dark:text-[#f5f5f5]">
-                  8
+                  {postCount}
                 </span>
                 <span className=" text-lg  text-gray-700 dark:text-[#f5f5f5]">
                   Post
@@ -55,7 +77,7 @@ function Profile() {
               </div>
               <div className="flex flex-col items-center cursor-pointer ">
                 <span className=" text-lg font-semibold dark:text-[#f5f5f5]">
-                  168
+                  {followerCount}
                 </span>
                 <span className="text-lg  text-gray-700 dark:text-[#f5f5f5]">
                   Followers
@@ -63,14 +85,18 @@ function Profile() {
               </div>
               <div className=" flex flex-col items-center cursor-pointer ">
                 <span className=" text-lg font-semibold dark:text-[#f5f5f5]">
-                  638
+                  {followingCount}
                 </span>
                 <span className=" text-lg  text-gray-700 dark:text-[#f5f5f5]">
                   Following
                 </span>
               </div>
             </div>
-            {user.uid === id ? <EditProfile /> : <FollowButton />}
+            {user.uid === id ? (
+              <EditProfile />
+            ) : (
+              <FollowButton postUserId={id} />
+            )}
           </div>
 
           {/* <!-- Name  --> */}
@@ -94,42 +120,47 @@ function Profile() {
                   size={20}
                 />
 
-                <p className="text-sm  md:text-base text-[#71767b] dark:text-gray-400">
-                  Joined{" "}
-                  <span>
+                <p className="text-sm md:text-base text-[#71767b] dark:text-gray-400">
+                  Joined
+                  <span className="ml-1">
                     {moment(currentUserProfile?.timestamp.toDate()).format(
                       "MMMM YYYY"
                     )}
                   </span>
                 </p>
               </div>
-              <BsLink45Deg
-                className=" text-[#71767b] dark:text-[#71767b]"
-                size={20}
-              />
-              <a
-                className="text-sm md:text-base text-sky-500 hover:underline"
-                href={currentUserProfile?.webUrl}
-                target="_blank"
-              >
-                {currentUserProfile?.webUrl.replace(/^(https?:\/\/)/, "")}
-                {/* instagram/lifewithjazz */}
-              </a>
+
+              {currentUserProfile?.webUrl && (
+                <div className="flex items-center">
+                  <BsLink45Deg
+                    className="text-[#71767b] dark:text-[#71767b]"
+                    size={20}
+                  />
+                  <a
+                    className="text-sm md:text-base text-sky-500 hover:underline ml-1"
+                    href={currentUserProfile?.webUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {currentUserProfile.webUrl.replace(/^(https?:\/\/)/, "")}
+                  </a>
+                </div>
+              )}
             </div>
           </div>
 
           {/* <!-- Tabs --> */}
           <div className="mt-3 flex justify-evenly md:hidden">
             <div className="flex flex-col w-full cursor-pointer items-center justify-center p-2 transition dark:text-[#f5f5f5] ">
-              <span className=" text-lg font-semibold">8</span>
+              <span className=" text-lg font-semibold">{postCount}</span>
               <span className=" text-lg dark:text-[#f5f5f5] ">Post</span>
             </div>
             <div className="flex flex-col w-full cursor-pointer items-center justify-center  dark:text-[#f5f5f5]  ">
-              <span className=" text-lg font-semibold">168</span>
+              <span className=" text-lg font-semibold"> {followerCount}</span>
               <span className="text-lg  dark:text-[#f5f5f5] ">Followers</span>
             </div>
             <div className="flex flex-col w-full cursor-pointer items-center justify-center  dark:text-[#f5f5f5] ">
-              <span className=" text-lg font-semibold">638</span>
+              <span className=" text-lg font-semibold">{followingCount}</span>
               <span className=" text-lg  dark:text-[#f5f5f5] ">Following</span>
             </div>
           </div>
@@ -137,7 +168,7 @@ function Profile() {
 
         {/* <!-- Photo Grid --> */}
         <div className="grid grid-cols-3 gap-1 justify-between mt-4 md:mt-7">
-          {documents && <PhotoGrid userPhotoDocs={documents} />}
+          {postDocs && <PhotoGrid userPhotoDocs={postDocs} />}
         </div>
       </div>
     </>
